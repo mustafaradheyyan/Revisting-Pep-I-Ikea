457035185e05fd6fdcad82cf9662c849262b0a70
mustafaradheyyan <31796217+mustafaradheyyan@users.noreply.github.com>
2023-05-24 10:03:31 -0700

    adding review files

--- test_database.py
+++ test_database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
 import dbconn
 
 db_username = dbconn.db_username
@@ -6,7 +7,7 @@ db_password = dbconn.db_password
 db_host = dbconn.db_host
 db_name = dbconn.db_name
 
-db_url = f'mysql+mysqlconnector://{db_username}:{db_password}@{db_host}/{db_name}'
+db_url = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}/{db_name}"
 
 engine = create_engine(db_url)
 conn = engine.connect()
@@ -15,29 +16,61 @@ result = conn.execute(text("select * from customers"))
 for person in result:
     print(person[0])
 
+
 def loginCustomer(email, password):
-    result = conn.execute(text(f"select customer_id from customers where email = '{email}' AND customer_pass = '{password}'"))
+    result = conn.execute(
+        text(
+            f"select customer_id from customers where email = '{email}' AND customer_pass = '{password}'"
+        )
+    )
     return_id = 0
     for res in result:
         return_id = res[0]
 
     return return_id
 
+
 def checkEmail(email):
     result = conn.execute(text(f"select * from customers where email = '{email}'"))
-    if (result.first() is None):
+    if result.first() is None:
         return True
     else:
         return False
-    
+
+
 def getAllProducts():
     result = conn.execute(text(f"select * from products"))
     return [r for r in result]
 
+
+def getAllReviews(user_id, sort_parameter=None, sort_method=None):
+    sql_sort_method_string = "desc" if sort_method == "🔼" else "asc"
+    sql_review_order_by_string = (
+        f" order by {sort_parameter} {sql_sort_method_string}" if sort_parameter else ""
+    )
+    sql_review_string = (
+        f"select name, price, num_stars from product_reviews join customer_product_review using(product_review_id)\
+                                join products using(product_id) where customer_id = {user_id}"
+        + sql_review_order_by_string
+    )
+
+    with Session(engine) as session:
+        result = session.execute(text(sql_review_string))
+    result_list = [r for r in result]
+    result_list = [
+        tuple(x if i != 2 else "⭐" * x for i, x in enumerate(result))
+        for result in result_list
+    ]
+    return result_list
+
+
 def getCustomerInfo(cust_id):
-    result = conn.execute(text(f"select * from customers where customer_id = {cust_id}"))
+    result = conn.execute(
+        text(f"select * from customers where customer_id = {cust_id}")
+    )
     return [r for r in result]
 
-if __name__ == '__main__':
-    loginCustomer('test@gmail.com','12345')
-    print(checkEmail('test123123@gmail.com'))
+
+if __name__ == "__main__":
+    loginCustomer("test@gmail.com", "12345")
+    print(checkEmail("test123123@gmail.com"))
--- website.py
+++ website.py
@@ -5,88 +5,163 @@ from wtforms.validators import InputRequired, Length, ValidationError
 import test_database
 import add_database
 
+
+
 app = Flask(__name__)
-app.secret_key = 'temp'
+app.secret_key = "temp"
+
+
 
 class RegisterForm(FlaskForm):
-    email = StringField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={'placeholder': 'Email'})
-    first_name = StringField(validators=[InputRequired(), Length(min=1, max=50)], render_kw={'placeholder': 'First Name'})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={'placeholder': 'Password'})
+    email = StringField(
+        validators=[InputRequired(), Length(min=4, max=50)],
+        render_kw={"placeholder": "Email"},
+    )
+    first_name = StringField(
+        validators=[InputRequired(), Length(min=1, max=50)],
+        render_kw={"placeholder": "First Name"},
+    )
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=50)],
+        render_kw={"placeholder": "Password"},
+    )
     submit = SubmitField("Register")
+
 
     def validate_email(self, email):
         if not test_database.checkEmail(email):
-            raise ValidationError("That email already exists. Please choose a different one.")
-    
+            raise ValidationError(
+                "That email already exists. Please choose a different one."
+            )
+
+
     def add_email(self, email, first_name, password):
         return add_database.addCustomer(email, first_name, password)
-        
+
+
+
 class LoginForm(FlaskForm):
-    email = StringField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={'placeholder': 'Email'})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={'placeholder': 'Password'})
+    email = StringField(
+        validators=[InputRequired(), Length(min=4, max=50)],
+        render_kw={"placeholder": "Email"},
+    )
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=50)],
+        render_kw={"placeholder": "Password"},
+    )
     submit = SubmitField("Login")
 
     def validate_login(self, email, password):
         user = test_database.loginCustomer(email, password)
         if user is None:
-            raise ValidationError("Login failed. Please check your email and password again.")
+            raise ValidationError(
+                "Login failed. Please check your email and password again."
+            )
         else:
             return user
 
+
+
+@app.route("/reviews/", methods=["GET", "POST"])
+def reviews():
+    if "user_id" in session:
+        sorted_status = ["🔽", "🔽"]
+        sort_parameter = None
+        sort_method = None
+        
+        if request.method == "POST":
+            sort_parameter = request.form["sort_parameter"]
+            sort_method = request.form["sort_method"]
+
+            match sort_parameter:
+                case "price":
+                    sorted_status = ["🔼", "🔽"]
+                case "num_stars":
+                    sorted_status = ["🔽", "🔼"]
+
+        reviews = test_database.getAllReviews(session["user_id"], sort_parameter, sort_method)
+
+        return render_template(
+            "reviews.html", reviews=reviews, sorted_status=sorted_status
+        )
+    else:
+        session["redirect"] = "reviews"
+        return redirect(url_for("login"))
+
+
+
 @app.route("/")
 def start():
-    if 'user_id' in session:
-        return redirect(url_for('home'))
+    if "user_id" in session:
+        return redirect(url_for("home"))
     else:
-        return redirect(url_for('login'))
+        session["redirect"] = "home"
+        return redirect(url_for("login"))
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if 'user_id' in session:
-        return redirect(url_for('home'))
+    if "user_id" in session:
+        return redirect(url_for("home"))
     form = LoginForm()
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
         try:
-            session['user_id'] = form.validate_login(email, password)
-            session['cart'] = []
-            return redirect(url_for('home'))
+            session["user_id"] = form.validate_login(email, password)
+            session["cart"] = []
+            return redirect(url_for(session["redirect"]))
         except Exception as e:
             print(e)
 
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
+
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if 'user_id' in session:
-        return redirect(url_for('home'))
+    if "user_id" in session:
+        return redirect(url_for("home"))
     form = RegisterForm()
-    if request.method == 'POST':
-        email = request.form['email']
-        first_name = request.form['first_name']
-        password = request.form['password']
+    if request.method == "POST":
+        print(request.form)
+        email = request.form["email"]
+        first_name = request.form["first_name"]
+        password = request.form["password"]
         try:
             form.validate_email(email)
-            session['user_id'] = form.add_email(email, first_name, password)
-            session['cart'] = []
-            return redirect(url_for('home'))
+            session["user_id"] = form.add_email(email, first_name, password)
+            session["cart"] = []
+            return redirect(url_for("home"))
         except Exception as e:
             print(e)
-            
-    return render_template('register.html', form=form)
+
+    return render_template("register.html", form=form)
+
+
 
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
+
+
 
 @app.route("/home")
 def home():
     html_table_format = []
     raw = test_database.getAllProducts()
-    #for x in raw:
-    return render_template('main.html', products=test_database.getAllProducts(), cartSize=session['cart'])
+    # for x in raw:
+    if "cart" in session:
+        return render_template(
+            "main.html",
+            products=test_database.getAllProducts(),
+            cartSize=session["cart"],
+        )
+    else:
+        return render_template("main.html", products=test_database.getAllProducts())
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app.run(debug=True)
